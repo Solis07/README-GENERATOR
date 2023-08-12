6e9245a70277aec96f7e285eaaedb00133d63646
L.J Solis <l.jsolis52@yahoo.com>
2023-08-12 14:27:06 -0500

    Listed the packages needed for this application in the TODO. I also created the structure for my array now I just need to impliment the remaining cateogires the assignment is requiring.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  
+  {
+    // Title of Project
+    type: 'input',
+    name: 'title',
+    message: 'Title of Project:',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please provide a project name.');
+        return false;
+      }
+    }
+  },
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
