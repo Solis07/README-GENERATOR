de1ead5b32dfb3283dd9b23cd10f8b1cf426a7bc
L.J Solis <l.jsolis52@yahoo.com>
2023-08-12 19:02:15 -0500

    minor changes

--- index.js
+++ index.js
@@ -103,11 +103,10 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+//function writeToFile(fileName, data) {}
   fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('README created!'));
-}
-
+    
 // TODO: Create a function to initialize app
 function init() {}
 
